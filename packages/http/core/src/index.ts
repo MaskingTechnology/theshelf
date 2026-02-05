bf15e7614cedd91b27915e96920f1ca55cc87f25
Peter van Vliet <peter@masking.tech>
2026-02-05 16:16:39 +0100

    #41: added mapped http driver

--- packages/http/core/src/index.ts
+++ packages/http/core/src/index.ts
@@ -5,6 +5,7 @@ export type * from './definitions/interfaces.js';
 
 export { default as HttpError } from './errors/HttpError.js';
 
+export { default as MappedDriver } from './drivers/Mapped.js';
 export { default as FetchDriver } from './drivers/Fetch.js';
 
 export { default } from './Http.js';
