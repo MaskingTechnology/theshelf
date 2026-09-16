89f91df89569871130e56227a65cfea623bc3d50
Peter van Vliet <peter@masking.tech>
2026-09-16 11:24:22 +0200

    #86: initial result definitions

---
+++ packages/database/core/src/MutationResult.ts
@@ -0,0 +1,25 @@
+
+export default class MutationResult
+{
+    readonly #affectedCount: number;
+
+    constructor(affectedCount: number)
+    {
+        this.#affectedCount = affectedCount;
+    }
+
+    get affectedCount(): number
+    {
+        return this.#affectedCount;
+    }
+
+    get noChanges(): boolean
+    {
+        return this.#affectedCount === 0;
+    }
+
+    get hasChanges(): boolean
+    {
+        return this.#affectedCount > 0;
+    }
+}
---
+++ packages/database/core/src/QueryResult.ts
@@ -0,0 +1,35 @@
+
+export default class QueryResult<T>
+{
+    readonly #records: T[];
+
+    constructor(records: T[])
+    {
+        this.#records = records;
+    }
+
+    get count(): number
+    {
+        return this.#records.length;
+    }
+
+    get filled(): boolean
+    {
+        return this.count > 0;
+    }
+
+    get empty(): boolean
+    {
+        return this.count === 0;
+    }
+
+    get record(): T | undefined
+    {
+        return this.#records[0];
+    }
+
+    get records(): T[]
+    {
+        return this.#records;
+    }
+}
